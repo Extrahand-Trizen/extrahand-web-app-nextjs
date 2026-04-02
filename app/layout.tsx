7324b0c54525948e15bd78a88f1ccd6d336db0ac
Asish <asishvenkat.a2004@gmail.com>
2026-04-02 12:59:45 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { QueryProvider } from "@/components/providers/QueryProvider";
 import { EnvironmentLogger } from "@/components/EnvironmentLogger";
 import { ScrollToTop } from "@/components/ScrollToTop";
-import { ConditionalPublicChrome } from "@/components/layout/ConditionalPublicChrome";
+
 
 const inter = Inter({
    subsets: ["latin"],
@@ -107,7 +107,7 @@ export default function RootLayout({
                         <ScrollToTop />
                      </Suspense>
                      <Toaster />
-                     <ConditionalPublicChrome>{children}</ConditionalPublicChrome>
+                     {children}
                   </AuthProvider>
                </QueryProvider>
             </ErrorBoundary>
