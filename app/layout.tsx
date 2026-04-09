44b67269c557008674cafb007f88096aacd3cc76
Asish <asishvenkat.a2004@gmail.com>
2026-04-09 12:42:28 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,9 +45,6 @@ export const metadata: Metadata = {
          },
       ],
    },
-   verification: {
-      google: "kJdq2hpjEv7FM_ffiqCPPLdzi2bcOFZGVIUXouVi3Jk",
-   },
    twitter: {
       card: "summary_large_image",
       title: "ExtraHand – Hire Help or Earn Money Near You",
